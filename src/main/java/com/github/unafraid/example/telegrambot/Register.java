5e7112875f3971d31208bd8ed68c2522c4ba402a
nizomovasadbek <48754111+nizomovasadbek@users.noreply.github.com>
2020-08-10 17:08:32 +0500

    Update and rename Main.java to Register.java

--- src/main/java/com/github/unafraid/example/telegrambot/Register.java
+++ src/main/java/com/github/unafraid/example/telegrambot/Register.java
@@ -2,7 +2,7 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class Main {
+public class Register {
     public static void main(String[] args){
         ApiContextInitializer.init();
         TelegramBotsApi bot = new TelegramBotsApi();
